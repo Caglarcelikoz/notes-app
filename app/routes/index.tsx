eb0782c89a1f20e0f405da564a6a8ce44320f2d7
Celiköz Caglar <caglar.celikoz@telenetgroup.be>
2023-01-25 21:42:13 +0100

    index changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,26 @@
+import { Link } from "@remix-run/react";
+import styles from "~/styles/home.css";
+
 export default function Index() {
   return (
     <main id="content">
-      <h1>Uw notities</h1>
+      <h1>Notes App</h1>
+
+      <h2>Documentation</h2>
+      <p>
+        <a href="https://remix.run/" target="_blank">
+          Remix.run
+        </a>
+      </p>
+      <p>
+        <a href="https://remix.run/blog/remix-vs-next" target="_blank">
+          Remix vs Next.js
+        </a>
+      </p>
     </main>
   );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
 }
